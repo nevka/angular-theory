93b59746140cd03f4180d1085e776a60fc109180
Алина <nevkaa@yandex.ru>
2018-11-23 18:49:33 +0300

    first component - delete

--- src/app/my/my.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-my',
-  templateUrl: './my.component.html'
-})
-export class MyComponent {
-    title = 'MyComponent!';
-}

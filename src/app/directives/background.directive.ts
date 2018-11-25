ca94ec61d6a43c51b70b3d63a4c50b51cfee6f4b
Алина <nevkaa@yandex.ru>
2018-11-25 13:24:02 +0300

    B4 - end

---
+++ src/app/directives/background.directive.ts
@@ -0,0 +1,18 @@
+import {Directive, ElementRef, OnInit} from '@angular/core';
+
+@Directive({
+    selector: '[appBackground]'
+})
+
+export class BackgroundDirective implements OnInit{
+    constructor(private element: ElementRef) {}
+
+    ngOnInit() {
+        // console.log(this.element);
+        this.element.nativeElement.style.backgroundColor = 'red';
+    }
+}
+
+
+// ng g d my-directive --spec=false
+

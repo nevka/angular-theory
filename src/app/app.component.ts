585d7a6735644187f87504a87082c7fc31d7fb34
Алина <nevkaa@yandex.ru>
2018-11-25 16:35:13 +0300

    B5 - 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,48 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="col-xs-8 col-xs-offset-2">
+        <h2>{{name | lowercase}}</h2>
+        <h2>{{name | uppercase}}</h2>
+        <h2>{{name | slice:0:3}}</h2>
+        <h2>{{name | slice:3:6 | uppercase}}</h2>
+        <hr />
+        <h2>{{pi}}</h2>
+        <h2>{{pi | number}}</h2>
+        <h2>{{pi | number:'1.3-4'}}</h2>
+        <h2>{{pi | number:'2.2-2'}}</h2>
+        <hr />
+        <h2>{{money | currency:'RUB'}}</h2>
+        <h2>{{money | currency:'USD':true}}</h2>
+        <hr />
+        <h2>{{date}}</h2>
+        <h2>{{date | date}}</h2>
+        <h2>{{date | date:'fullDate'}}</h2>
+        <h2>{{date | date:'shortTime'}}</h2>
+        <hr />
+        <h2>{{amount}}</h2>
+        <h2>{{amount | percent}}</h2>
+        <hr />
+        <h2>{{object}}</h2>
+        <h2>{{object | json}}</h2>
+        <pre>{{object | json}}</pre>
+    </div>
+  `
 })
 export class AppComponent {
-  headerText = 'Создание своей директивы.';
-  items = [1, 2, 3];
-  current = 1;
-
-  onClick(number: number) {
-      this.current = number;
-  }
+  name = 'WebForMyself';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested:
+      {
+        xyz: 3,
+        numbers: [1, 2, 3]
+      }
+  };
 }
